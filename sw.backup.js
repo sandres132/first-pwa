adda6d8a3e9fc47db0c2782c9d46718a2676a504
andres <60414032+sandres132@users.noreply.github.com>
2025-03-01 17:25:23 -0600

    fin del 02-service-worker

--- sw.backup.js
+++ sw.backup.js
@@ -31,4 +31,30 @@ self.addEventListener('fetch', event => {
     //     let fotoReq = fetch( 'img/main-patas-arriba.jpg' );
     //     event.respondWith( fotoReq );
     // }
+    
+    // event.respondWith( 
+    //     fetch( event.request )
+    //         .then( resp => {
+
+    //             if ( resp.ok ) {
+    //                 return resp;
+    //             } else {
+    //                 return fetch('img/main.jpg');
+    //             }
+    //
+    //             simplificacion del if else
+    //             if ( resp.ok ) return resp; 
+    //             else return fetch('img/main.jpg');
+    //         })
+
+            // simplificacion del .then
+            // .then( resp => {
+            //     return resp.ok ? resp : fetch('img/main.jpg');
+            // });
+    // )
+
+    // const resp = fetch( event.request )
+    //     .then( resp => resp.ok ? resp : fetch('img/main.jpg'));
+
+    // event.respondWith( resp );
 });
--- sw.js
+++ sw.js
@@ -2,27 +2,6 @@
 
 self.addEventListener('fetch', event => {
 
-    // event.respondWith( 
-    //     fetch( event.request )
-    //         .then( resp => {
-
-    //             if ( resp.ok ) {
-    //                 return resp;
-    //             } else {
-    //                 return fetch('img/main.jpg');
-    //             }
-    //
-    //             simplificacion del if else
-    //             if ( resp.ok ) return resp; 
-    //             else return fetch('img/main.jpg');
-    //         })
-
-            // simplificacion del .then
-            // .then( resp => {
-            //     return resp.ok ? resp : fetch('img/main.jpg');
-            // });
-    // )
-
     const resp = fetch( event.request )
         .then( resp => resp.ok ? resp : fetch('img/main.jpg'));
 
